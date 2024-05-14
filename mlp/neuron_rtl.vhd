c2e594ae5d985262996bb01420b9fa3d20c79e4a
gpetrousov <petrousov@gmail.com>
2024-05-14 16:07:56 +0200

    Use double loop for neuron output

--- mlp/neuron_rtl.vhd
+++ mlp/neuron_rtl.vhd
@@ -1,3 +1,8 @@
+-- nn_logic library
+package nn_io_logic is new work.nn_logic
+  generic map(m => 8, n => 8);
+use work.nn_io_logic.all;
+
 -------------- Std Libraries
 library ieee;
 use ieee.std_logic_1164.all;
@@ -7,15 +12,13 @@ LIBRARY ieee;
 Library floatfixlib; 
 use floatfixlib.fixed_pkg.all;
 
--- nn_logic library
-use work.nn_logic.all;
-
 
 -------------- Entity
 
 entity neuron is
   generic(
-    nof_in_features : integer := 2);
+    nof_in_features : integer := 2 -- To be 784
+  );
 
   port(
    -- control ports
@@ -27,38 +30,42 @@ entity neuron is
     in_weights  : in nn_io_matrix(nof_in_features - 1 downto 0);
 
     -- output ports
-    outputs : out nn_io_vector_prod
+    output : out sfixed(17 downto -16)
   );
 end neuron; 
 
 -------------- Architecture
 
 architecture rtl of neuron is
-  signal in_features_sig : nn_io_matrix(nof_in_features -1 downto 0)  := (others => (others => '0'));
-  signal in_weights_sig  : nn_io_matrix(nof_in_features - 1 downto 0) := (others => (others => '0'));
+  type prod_sig_matrix is array(nof_in_features -1 downto 0) of sfixed(17 downto -16);
+  subtype sum_sig_vector is sfixed(17 downto -16);
+
+  signal in_features_sig : nn_io_matrix(nof_in_features - 1 downto 0)  := in_features;
+  signal in_weights_sig  : nn_io_matrix(nof_in_features - 1 downto 0)  := in_weights;
+
+  signal prod_sig        : prod_sig_matrix                             := (others => (others =>'0'));
+  signal sum_sig         : sum_sig_vector                              := (others => '0');
 begin
 
-  reset : process(clk, rst) is
+  process(clk) is
+
   begin
     if rising_edge(clk) then
-      if rst = '1' then
-        in_features_sig <= (others => (others => '0'));
-        in_weights_sig  <= (others => (others => '0'));
-      else
-        in_features_sig <= in_features;
-        in_weights_sig  <= in_weights;
-      end if;
+
+      -- MAC here
+      for i in 0 to nof_in_features-1 loop
+        report "Multiply Loop : " & "i=" & integer'image(i);
+        prod_sig(i) <= in_features_sig(i) * in_weights_sig(i);
+      end loop;
+
+      for j in 0 to nof_in_features-1 loop
+        report "Sum Loop: " & "i=" & integer'image(j);
+        sum_sig <= resize(sum_sig + prod_sig(j), 17, -16);
+      end loop;
+
     end if;
   end process;
 
-  SoP : process(in_features_sig) is
-    variable sum_prod : nn_io_vector_prod;
-  begin
-    if rising_edge(clk) then
-      product_loop: for i in 1 to nof_in_features loop
-        sum_prod := sum_prod + in_features(i) * in_weights(i);
-      end loop;
-    end if;
-    outputs <= sum_prod;
-  end process;
+  -- Output from entity
+  output <= sum_sig;
 end architecture;
