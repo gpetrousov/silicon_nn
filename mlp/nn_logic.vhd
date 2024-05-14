4ccd29724873cc12d66623bf55a2bfb52585d29f
gpetrousov <petrousov@gmail.com>
2024-05-14 16:08:07 +0200

    Make library generic

--- mlp/nn_logic.vhd
+++ mlp/nn_logic.vhd
@@ -14,13 +14,12 @@ library floatfixlib;
 use floatfixlib.fixed_pkg.all;
 
 package nn_logic is
-
-  -- Qm,n sizes
-	constant m: integer := 16; --! Size of the integer part
-	constant n: integer := 16; --! Size of the fractionary part
+  generic(
+    m : integer := 8;
+    n : integer := 8
+  );
 
   -- sfixed type extensions
-  subtype nn_io_vector_prod is sfixed(2*m + 1 downto -n);
   subtype nn_io_vector is sfixed(m downto -n);
   type nn_io_matrix is array (integer range <>) of nn_io_vector;
 
