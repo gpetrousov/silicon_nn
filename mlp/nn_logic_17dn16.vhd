3216d63bef3a7c5e546b86916f874620873bf92b
gpetrousov <petrousov@gmail.com>
2024-05-30 16:31:04 +0200

    Fix package name

--- mlp/nn_logic_17dn16.vhd
+++ mlp/nn_logic_17dn16.vhd
@@ -13,10 +13,10 @@ use ieee.numeric_std.all;
 library floatfixlib;
 use floatfixlib.fixed_pkg.all;
 
-package nn_logic_17dn16 is
+package nn_io_logic_17dn16bit is
 
   -- sfixed type extensions
   subtype nn_io_vector is sfixed(17 downto -16);
   type nn_io_matrix is array (integer range <>) of nn_io_vector;
 
-end package nn_logic_17dn16;
+end package nn_io_logic_17dn16bit;
